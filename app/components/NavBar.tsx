9a00df07ba8dfa68fa8d3c5f2b0e6f2b065e2edd
Sebastian320CA <sebastian022231@gmail.com>
2026-06-02 20:44:23 -0500

    Comienzo del portafolio web

---
+++ app/components/NavBar.tsx
@@ -0,0 +1,12 @@
+const navBar = () => {
+    return (
+        <nav>
+            <ul>
+                <li><a href="#home">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+            </ul>
+        </nav>
+    )
+}
+export default navBar
